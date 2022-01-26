55766c404bce569b320c76e7a85b313d567ea4cf
Ivan de Andrade Santana <ivanandradesantana@gmail.com>
2022-01-25 22:40:34 -0300

    useState, useRouter

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import appConfig from '../config.json';
+import React from 'react';
+import { useRouter } from 'next/router';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 
 function Titulo(props)
@@ -19,40 +21,6 @@ function Titulo(props)
     )
 }
 
-function GlobalStyle()
-{
-    return(
-        <style global jsx>{`
-                * {
-                    margin: 0;
-                    padding: 0;
-                    box-sizing: border-box;
-                    list-style: none;
-                }
-
-                body {
-                    font-family: 'Open Sans', sans-serif;
-                }
-
-                /* App fit Height */ 
-                html, body, #__next {
-                    min-height: 100vh;
-                    display: flex;
-                    flex: 1;
-                }
-
-                #__next {
-                    flex: 1;
-                }
-
-                #__next > * {
-                    flex: 1;
-                }
-                /* ./App fit Height */ 
-            `}
-        </style>)
-}
-
 //function HomePage()
 //{
 //   return(
@@ -67,11 +35,11 @@ function GlobalStyle()
 
 export default function PaginaInicial() 
 {
-    const username = 'ivanandradesantana';
-  
+    const [username, setUserName] = React.useState('ivanandradesantana');
+    const roteamento = useRouter();
+
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -97,11 +65,15 @@ export default function PaginaInicial()
           >
             {/* Formulário */}
             <Box
-              as="form"
-              styleSheet={{
-                display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-                width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
-              }}
+                as="form"
+                onSubmit={function(infosDoEvento){
+                    infosDoEvento.preventDefault();
+                    roteamento.push('/chat');
+                }}
+                styleSheet={{
+                    display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                    width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+                }}
             >
               <Titulo tag="h2">Boas vindas de volta!</Titulo>
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
@@ -109,6 +81,20 @@ export default function PaginaInicial()
               </Text>
   
               <TextField
+                value={username}
+                onChange={function (event){
+                        const valor = event.target.value;
+                        
+                        if(valor.length <= 2)
+                        {
+                            setUserName(null);
+                        }
+                        else
+                        {
+                            setUserName(valor);
+                        }
+                    }
+                }
                 fullWidth
                 textFieldColors={{
                   neutral: {
